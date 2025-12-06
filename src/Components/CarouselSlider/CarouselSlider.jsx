bccac05cbbb763b8290053a89ab9f4d2197db45d
shaharearhossainjony <shaharearhossainjony@gmail.com>
2025-12-07 03:47:12 +0600

    Banner Images updated and typewriter text color change

--- src/Components/CarouselSlider/CarouselSlider.jsx
+++ src/Components/CarouselSlider/CarouselSlider.jsx
@@ -1,16 +1,15 @@
-
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
-import { Navigation } from "swiper/modules";
+import { Navigation, Autoplay } from "swiper/modules";
 import { Typewriter } from "react-simple-typewriter";
 
 const HeroSlider = () => {
   const images = [
-    "https://i.ibb.co.com/ZpY0tZWg/1.jpg",
-    "https://i.ibb.co.com/k4wrYpS/adopt2.jpg",
-    "https://i.ibb.co.com/tp06nQj/owner-happy.jpg"
+    "https://i.ibb.co.com/MyjFDSkD/download-13.jpg",
+    "https://i.ibb.co.com/QvgX038s/download-12.jpg",
+    "https://i.ibb.co.com/h1rLjjfG/images-3.jpg"
   ];
 
   const taglines = [
@@ -23,22 +22,21 @@ const HeroSlider = () => {
     <div className="pt-8 pb-8 px-4">
       <Swiper
         navigation={true}
-        modules={[Navigation]}
+        modules={[Navigation, Autoplay]}
+        autoplay={{ delay: 6000, disableOnInteraction: false }}
+        loop={true}
         className="rounded-xl"
       >
         {images.map((img, index) => (
           <SwiperSlide key={index}>
             <div className="relative">
-              
               <img
                 src={img}
                 className="w-full h-[420px] object-cover rounded-xl"
                 alt="pet"
               />
-
               <div className="absolute inset-0 bg-black/40 rounded-xl"></div>
-
-              <h1 className="absolute top-1/2 left-10 -translate-y-1/2 text-3xl md:text-5xl font-bold text-white w-[70%] drop-shadow-lg">
+              <h1 className="absolute top-1/2 left-10 -translate-y-1/2 text-3xl md:text-5xl font-bold text-amber-400 w-[70%] drop-shadow-lg">
                 <Typewriter
                   words={[taglines[index]]}
                   loop={0}
